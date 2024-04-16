387fdd62b7ad93503e143452039f299b193b4901
peter-fh <p.felty.howe@gmail.com>
2024-04-16 02:32:01 -0400

    added fen function to engine

--- public/engine/Driver.cpp
+++ public/engine/Driver.cpp
@@ -3,147 +3,25 @@
 
 
 using std::cout;
-int passes = 0;
+using std::cin;
 
-void dfs(Board *chess_board, int depth){
+class Driver {
+public:
+    static void run(){
+	string inp_fen;
+	getline(cin, inp_fen);
+	Board board(inp_fen);
+	Move* engineMove = new Move();
+	min(&board, 4, engineMove);
+	board.makeMove(*engineMove);
+	cout << board.fen();
 
-    if (depth == 0) {
-	passes++;
-	//cout << chess_board->toString();
-	return;
     }
+};
 
-    Moves board_moves = chess_board->getMoves();
-    while (board_moves.hasNext()){
-
-	Move current_move = board_moves.getMove();
-	
-	chess_board->makeMove(current_move);
-	dfs(chess_board, depth - 1);
-	chess_board->unmakeMove(current_move);
-
-	board_moves.next();
-    }
-
-}
-
-
-void dfsDebug(Board *chess_board, int depth){
-
-    // cout << "\nBoard (depth " << depth << "):\n";
-    // cout << chess_board->toString();
-    if (depth == 0) {
-	return;
-    }
-
-    Moves board_moves = chess_board->getMoves();
-    // board_moves.displayMoves();
-    // cout << "\n";
- 
-    //while (chess_board->validBoardState() && board_moves.hasNext()){
-    while (board_moves.hasNext()){
-	if (!chess_board->validBoardState()){
-	    cout << chess_board->toString();
-	    cout << "Invalid state. Exiting.\n";
-	    exit(0);
-	} 
-	Move current_move = board_moves.getMove();
-	if (!chess_board->isLegal(current_move)){
-	    // cout << "Illegal move found\n";
-	} 
-	
-	chess_board->makeMove(current_move);
-	// cout << current_move.moveCode();
-	dfsDebug(chess_board, depth - 1);
-	chess_board->unmakeMove(current_move);
-	board_moves.next();
-    }
-
-}
-
-void playGame(){
-    Board board = Board("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
-    Board *chess_board = &board;
-    Moves board_moves = chess_board->getMoves();
-    while (board_moves.length() != 0 ){
-	// cout << "BEFORE MOVES:\n";
-	cout << chess_board->toString();
-	if (!chess_board->validBoardState()){
-	    cout << "Invalid state. Exiting.\n";
-	    exit(0);
-	}
-	Move random_move = board_moves.randomMove();
-	chess_board->makeMove(random_move);
-	// cout << "AFTER 1 MOVE:\n";
-	// cout << chess_board->toString();
-	// chess_board->unmakeMove(random_move);
-	// cout << "AFTER UNDO MOVE:\n";
-	// cout << chess_board->toString();
-	// chess_board->makeMove(random_move);
-	// cout << "AFTER 1 MOVE:\n";
-	// cout << chess_board->toString();
-	
-	board_moves = chess_board->getMoves();
-    }
-}
-void debugMoves(int depth){
-    Board board("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
-    dfs(&board, depth);
-    if (!board.validBoardState()){
-	cout << "Invalid board state!\n";
-    }
-
-}
-
-
-void tryTwoMoves(){
-
-    Board board("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
-    Board* chess_board = &board;
-    Move *move1 = new Move(8, 16, 5, false);
-    Move *move2 = new Move(48, 40, 5, false);
-    Move *move3 = new Move(16, 24, 5, false);
-
-    chess_board->makeMove(*move1);
-    cout << "First move:\n";
-    cout << chess_board->toString();
-
-    chess_board->makeMove(*move2);
-    cout << "Second move:\n";
-    cout << chess_board->toString();
-
-    chess_board->makeMove(*move3);
-    cout << "Third move:\n";
-    cout << chess_board->toString();
-
-    chess_board->unmakeMove(*move3);
-    cout << "Unmade move 3:\n"; 
-    cout << chess_board->toString();
-
-    chess_board->unmakeMove(*move2);
-    cout << "Unmade move 2:\n"; 
-    cout << chess_board->toString();
-
-    chess_board->unmakeMove(*move1);
-    cout << "Unmade move 1:\n"; 
-    cout << chess_board->toString();
-}
-
-
+// "K7/8/3Q4/8/8/3q4/8/k7 w - - 0 1"
 int main(){
-    //debugMoves(6);
-    //tryTwoMoves();
-    //playGame();
-    //cout << "Number of leaf games: " << passes << "\n";
-    //Board board("2qkqn2/8/8/8/8/8/1QRQQQ2/K7 w KQkq - 0 1");
-    Board board("K7/8/3Q4/8/8/3q4/8/k7 w - - 0 1");
-    cout << "Board:\n";
-    cout << board.toString();
-
-    Move* engineMove = new Move();
-    cout << min(&board, 4, engineMove) << "\n";
-    cout << engineMove->moveCode();
-
+    Driver::run();
     
     return 0;
 }
