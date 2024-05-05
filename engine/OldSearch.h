61d7b0410fc2423cb06038ed52b29301c382670a
Disputatio <p.felty.howe@gmail.com>
2024-05-05 17:35:29 -0400

    removed old seach

--- engine/OldSearch.h
+++
@@ -1,9 +0,0 @@
-#include "Board.h"
-
-
-Move* minimax(Board *board, int depth);
-Move* max_root(Board *board, int depth);
-Move* min_root(Board *board, int depth);
-int max(Board *board, int depth);
-int min(Board *board, int depth);
-
